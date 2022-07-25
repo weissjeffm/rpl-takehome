9c294b1bb8a4ca2c3199e983dcdb80268e20c0de
Jeff Weiss <me@jweiss.com>
2022-07-25 09:39:23 -0400

    Add org file with description of project

---
+++ test/redplanetlabs/core_test.clj
@@ -0,0 +1,7 @@
+(ns redplanetlabs.core-test
+  (:require [clojure.test :refer :all]
+            [redplanetlabs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
